2fea190f0ed37cd47cb4a81038925484ec68975a
Nelson Arevalo <nelson.arevalo.tecni@gmail.com>
2020-11-25 16:50:33 +0000

    added: strip function

---
+++ aux_functions.c
@@ -0,0 +1,25 @@
+#include "shell.h"
+
+/**
+ * strip_line - remove the new line character
+ * @line: input got from getline
+ * Return: no return
+ */
+
+static void strip_line(char *line)
+{
+	size_t len = _strlen(line);
+	size_t end = len - 1;
+
+	while (1)
+	{
+		if (line[end] == '\n')
+			line[end] = '\0';
+		if (end == 0)
+			break;
+		end--;
+	}
+
+	if (line[end] == '\n')
+		line[end] = '\0';
+}
