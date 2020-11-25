1d27a7044e48622c0b1de169679f297b507ca135
Jhon Arias <7honarias@gmail.com>
2020-11-25 17:16:42 +0000

    [UPDATE] - new file for str functions

---
+++ str_funtions.c
@@ -0,0 +1,14 @@
+/**
+ * _strlen - return the length the a string
+ * @s: is the pointer the variable "n"
+ * Return: Always 1 (Success)
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+	}
+	return (i);
+}
