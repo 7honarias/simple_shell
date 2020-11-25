28896faf2fa02860cfc6ab72fc1ee87f7b1803a5
Jhon Arias <7honarias@gmail.com>
2020-11-25 18:16:44 +0000

    [UPDATE] - prototype of functios

--- shell.h
+++ shell.h
@@ -3,5 +3,17 @@
 
 #include <errno.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#define TOKEN_LIMIT 1024
+void print_prompt(void);
+int read_line(char **input);
+static void strip_line(char *line);
+static int tokenize_line(char *line, char ***tokens);
+int eval(char *input);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+char *_strdup(char *src);
 
 #endif
