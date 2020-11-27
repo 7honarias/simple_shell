c29ac6d631cc1a30f678bbd686937da259200628
Nelson Arevalo <nelson.arevalo.tecni@gmail.com>
2020-11-27 02:42:52 +0000

    fixed the main function ans header functions

--- main.c
+++ main.c
@@ -1,35 +1,67 @@
 #include "shell.h"
+
 /**
  * main - funtion main
  * @argc: number of arguments but for now not passed arguments
  * @argv: the array of pointers to fill with set_argv
  * Return: Always 0
  */
+
 int main(int argc, char *argv[])
 {
-	int err = 0;
-	char *input = 0;
+	path_s *header_PATH = NULL;
+	int status, err = 0;
+	ssize_t sizes_line = 0;
+	char *input = NULL, **tokens = NULL;
+	size_t len = 0;
 
-	do {
-		input = 0;
-		/*print_prompt();*/
+	built_in builtin_list[] = {{"exit", f_exit}, {"env", f_env}, {"NULL", NULL}};
+	tofree_s tofree;
+
+	tofree.f_buff_line = &input;
+	tofree.f_header_PATH = &header_PATH;
+	tofree.f_arguments = &tokens;
+	tokens = malloc_arguments();
+	header_PATH = create_linkedlist_path(_getenv("PATH"));
+	while (1)
+	{
 		if (isatty(STDIN_FILENO))
 			write(STDOUT_FILENO, "$ ", 2);
-/* read*/
-		err = read_line(&input);
-		if (err < 0)
+		err++;
+		sizes_line = getline(&input, &len, stdin);
+		if (sizes_line < 0)
 		{
-			free(input);
+			free_all(tofree);
 			if (isatty(STDIN_FILENO))
 				write(1, "\n", 1);
 			exit(EXIT_SUCCESS);
 		}
-/* eval*/
-		err = eval(input, argv[0]);
-		free(input);
-/* loop*/
-	} while (!err);
-
+		tokenize_line(input, tokens);
+		if (tokens[0] != NULL)
+			if (_builtin(tokens, builtin_list, err, tofree, argv[0]) == 0)
+				if (fork() == 0)
+					if (eval(header_PATH, tokens) != 0)
+						print_error(err, argv[0], tokens[0]);
+		wait(&status);
+	}
 	(void)argc;
-	return (0);
+	return (WEXITSTATUS(status));
+}
+
+/**
+ * malloc_arguments - function to set memory to arguments
+ * Return: arguments
+ */
+
+char **malloc_arguments(void)
+{
+	char **tokens = NULL;
+
+	tokens = malloc(sizeof(tokens) * 120);
+	if (tokens == NULL)
+	{
+		_puts("fail memory allocation\n");
+		exit(EXIT_FAILURE);
+	}
+	return (tokens);
 }
--- shell.h
+++ shell.h
@@ -8,15 +8,70 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+
 #define TOKEN_LIMIT 1024
-int read_line(char **input);
+
+/**
+ * struct path - struct path
+ * @s: it's the string
+ * @next: it's the next node
+ */
+
+typedef struct path
+{
+	char *s;
+	struct path *next;
+} path_s;
+
+/**
+ * struct _free - struct that will have the pointers to free
+ * @f_header_PATH: linked list that will have the PATH
+ * @f_buff_line: string set by get line
+ * @f_arguments: arguments to free
+ */
+
+typedef struct _free
+{
+	path_s **f_header_PATH;
+	char **f_buff_line;
+	char ***f_arguments;
+} tofree_s;
+
+/**
+ * struct built - the struct to the built-ins
+ * @name: the name of the built-in
+ * @func: the function to the built-in
+ */
+
+typedef struct built
+{
+	char *name;
+	int (*func)(char **argv, int c, tofree_s tofree, char *n);
+} built_in;
+
+/*PROTYPE FUNCTIONS*/
+
 void strip_line(char *line);
-int tokenize_line(char *line, char ***tokens);
-int eval(char *input, char *name);
-int _strlen(char *s);
+void tokenize_line(char *input, char **tokens);
+int eval(path_s *head, char *const tokens[]);
+/* print error */
+char *tostring(char *scount, int count);
+void print_error(int count, char *name, char *command);
 int _puts(char *str);
-void print_error(char *name);
 char *_strcpy(char *dest, char *src);
-char *_strdup(char *src);
+int _strlen(char *s);
+/*path built in*/
+int f_exit(char **argv, int c, tofree_s tofree, char *n);
+int f_env(char **argv, int c, tofree_s tofree, char *n);
+int _builtin(char **argv, built_in *b, int c, tofree_s t, char *n);
+int _strcmp(char *s1, char *s2);
+/*path*/
+path_s *create_linkedlist_path(char *path);
+void _new_node(char *s, path_s **head);
+path_s *create_node(char *s);
+void free_PATH(path_s *head);
+char *_getenv(const char *name);
+char **malloc_arguments(void);
+void free_all(tofree_s tofree);
 
 #endif
